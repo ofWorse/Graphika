2f27733dc2452ad3b1f6bac5660db835c1737971
Danler-rs <ilyakors2003@gmail.com>
2024-04-08 22:09:02 +0300

    pythonconveyor: add ability to send data to python function via resource path #0020

--- pythonconveyor.h
+++ pythonconveyor.h
@@ -1,70 +1,51 @@
 #ifndef PYTHONCONVEYOR_H
 #define PYTHONCONVEYOR_H
-#include <Python.h>
-#include <iostream>
+
+
+#include <QObject>
+#include <QString>
 #include <vector>
-#include <string>
-#include <stdexcept>
-#include <sstream>
 
-class PythonConveyor
+class PythonConveyor : public QObject
 {
-private:
-    PyObject* pName;
-    PyObject* pModule;
-    PyObject* pDict;
-    PyObject* pObjct;
-    PyObject* pVal;
-    PyObject* sys;
-    PyObject* sys_path;
-    PyObject* folder_path;
-
-    std::string m_folderPath;
-    std::string m_moduleName;
-    std::string m_functionName;
-    std::string m_result;
-
-    std::vector<double> x;
-    std::vector<double> y;
+    Q_OBJECT
 
 public:
-    PythonConveyor( const std::string& folderPath, const std::string& moduleName, const std::string& functionName );
-    ~PythonConveyor();
+    explicit PythonConveyor( QObject* parent = nullptr );
+    PythonConveyor( const QString& pythonFilePath, const QString& functionName, double precision, QObject* parent = nullptr );
 
-    void setDataX( const std::vector<double>& x )
-    {
-        this->x = x;
-    }
+    void setPythonFilePath( const QString& pythonFilePath );
+    QString getPythonFilePath() const;
 
-    void setDataY( const std::vector<double>& y )
-    {
-        this->y = y;
-    }
+    void setFunctionName( const QString& functionName );
+    QString getFunctionName() const;
 
-    void setFolderPath( const std::string& folderPath );
-    void setModuleName( const std::string& moduleName );
-    void setFunctionName( const std::string& functionName );
-    std::string getResult( void );
+    void setPrecision( double precision );
+    double getPrecision() const;
 
-    void initialize( void );
+    void setDataX( const std::vector< double >& vector );
+    std::vector< double > get_X_Vector() const;
 
-    std::vector<std::string> convertDoubleToString( const std::vector<double>& input )
-    {
-        std::vector<std::string> output;
+    void setDataY( const std::vector< double >& vector );
+    std::vector< double > get_Y_Vector() const;
 
-        for ( const auto& value : input )
-        {
-            std::ostringstream stream;
-            stream << value;
-            output.push_back( stream.str() );
-        }
+    void setResult( const QString& result );
+    QString getResult() const;
 
-        return output;
-    }
+    QStringList convertVectorToStringList( const std::vector< double >& inputVector );
 
-    void clear( void );
-    void sendArraysToPythonFunction(  );
-    void checkArraysSizes( const std::vector< std::string >& array1, const std::vector< std::string >& array2 );
+    void sendArraysToPythonFunction();
+    QString getResourceFilePath( const QString& resourcePath );
+
+private:
+    QString m_pythonFilePath;
+    QString m_functionName;
+    double m_precision;
+    std::vector< double > m_xVector;
+    std::vector< double > m_yVector;
+    QString m_result;
+
+    bool isResourcePath( const QString& path );
 };
 
 #endif // PYTHONCONVEYOR_H
---
+++ resources/pymodules/polynomials.py
@@ -0,0 +1,79 @@
+import sympy
+from sympy import symbols
+
+
+def cast_to_number(x, y):
+    contains_float = False
+    for i in range(len(x)):
+        x[i] = sympy.core.Number(x[i])
+        y[i] = sympy.core.Number(y[i])
+    for i in range(len(x)):
+        if x[i].is_Float or y[i].is_Float:
+            contains_float = True
+            break
+    if contains_float:
+        for i in range(len(x)):
+            x[i] = sympy.core.Float(x[i])
+            y[i] = sympy.core.Float(y[i])
+    return x, y, contains_float
+
+
+def newton_polynomial(x_values, y_values, precision=4):
+    x_values, y_values, contains_float = cast_to_number(x_values, y_values)
+    n = len(x_values)
+    polynomial = y_values[0]
+    x = symbols("x")
+    for polynomial_term in range(1, n):
+        multiplier = 1
+        for j in range(polynomial_term):
+            multiplier *= (x - x_values[j])
+        polynomial += (divided_difference(polynomial_term, x_values[:polynomial_term + 1],
+                                          y_values[:polynomial_term + 1])) * multiplier
+    if contains_float:
+        return str(sympy.sympify(sympy.cancel(polynomial, x), rational=None).evalf(n=precision)).replace("**", "^")
+    return str(sympy.cancel(polynomial, x)).replace("**", "^")
+
+
+def divided_difference(n, x_values, y_values):
+    if n == 1:
+        return sympy.Rational((y_values[n] - y_values[n - 1]), (x_values[n] - x_values[0]))
+    else:
+        return sympy.Rational((divided_difference(n - 1, x_values[1:], y_values[1:]) -
+                               divided_difference(n - 1, x_values[:n], y_values[:n])), (x_values[n] - x_values[0]))
+
+
+def lagrange_polynomial(x_values, y_values, precision=4):
+    x_values, y_values, contains_float = cast_to_number(x_values, y_values)
+    polynomial = 0
+    n = len(x_values)
+    x = symbols("x")
+    for i in range(n):
+        lagrange = 1
+        for j in range(n):
+            if j == i:
+                continue
+            lagrange *= (x - x_values[j]) / (x_values[i] - x_values[j])
+        polynomial += y_values[i] * lagrange
+    if contains_float:
+        return str(sympy.sympify(sympy.cancel(polynomial, x), rational=None).evalf(n=precision)).replace("**", "^")
+    return str(sympy.cancel(polynomial, x)).replace("**", "^")
+
+
+def berruta_functions(x_values, y_values, precision=4):
+    x_values, y_values, contains_float = cast_to_number(x_values, y_values)
+    numerator = 0
+    denominator = 0
+    n = len(x_values)
+    x = symbols("x")
+    for k in range(n):
+        product = 1
+        for i in range(n):
+            if i == k:
+                continue
+            product *= (x - x_values[i])
+        numerator += (-1) ** k * y_values[k] * product
+        denominator += (-1) ** k * product
+    polynomial = numerator / denominator
+    if contains_float:
+        return str(sympy.sympify(sympy.cancel(polynomial, x), rational=None).evalf(n=precision)).replace("**", "^")
+    return str(sympy.cancel(polynomial, x)).replace("**", "^")
