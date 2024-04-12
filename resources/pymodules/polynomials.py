1d5b386a391ea1e6f6ce622a4877c939b9d60ca5
Danler-rs <ilyakors2003@gmail.com>
2024-04-12 21:00:40 +0300

    pymodules: update python module polynomials #0032

--- resources/pymodules/polynomials.py
+++ resources/pymodules/polynomials.py
@@ -3,22 +3,18 @@ from sympy import symbols
 
 
 def cast_to_number(x, y):
+    x_act = list()
+    y_act = list()
     contains_float = False
     for i in range(len(x)):
-        x[i] = sympy.core.Number(x[i])
-        y[i] = sympy.core.Number(y[i])
-    for i in range(len(x)):
-        if x[i].is_Float or y[i].is_Float:
+        if "." in x[i] or "." in y[i]:
             contains_float = True
-            break
-    if contains_float:
-        for i in range(len(x)):
-            x[i] = sympy.core.Float(x[i])
-            y[i] = sympy.core.Float(y[i])
-    return x, y, contains_float
+        x_act.append(sympy.core.Rational(x[i]))
+        y_act.append(sympy.core.Rational(y[i]))
+    return x_act, y_act, contains_float
 
 
-def newton_polynomial(x_values, y_values, precision=4):
+def newton_polynomial(x_values, y_values, precision=2):
     x_values, y_values, contains_float = cast_to_number(x_values, y_values)
     n = len(x_values)
     polynomial = y_values[0]
@@ -27,10 +23,9 @@ def newton_polynomial(x_values, y_values, precision=4):
         multiplier = 1
         for j in range(polynomial_term):
             multiplier *= (x - x_values[j])
-        polynomial += (divided_difference(polynomial_term, x_values[:polynomial_term + 1],
-                                          y_values[:polynomial_term + 1])) * multiplier
+        polynomial += divided_difference(polynomial_term, x_values[:polynomial_term + 1], y_values[:polynomial_term + 1]) * multiplier
     if contains_float:
-        return str(sympy.sympify(sympy.cancel(polynomial, x), rational=None).evalf(n=precision)).replace("**", "^")
+        return str(sympy.nsimplify(polynomial.cancel().evalf(), tolerance=10**-precision)).replace("**", "^")
     return str(sympy.cancel(polynomial, x)).replace("**", "^")
 
 
@@ -42,9 +37,9 @@ def divided_difference(n, x_values, y_values):
                                divided_difference(n - 1, x_values[:n], y_values[:n])), (x_values[n] - x_values[0]))
 
 
-def lagrange_polynomial(x_values, y_values, precision=4):
+def lagrange_polynomial(x_values, y_values, precision=2):
     x_values, y_values, contains_float = cast_to_number(x_values, y_values)
-    polynomial = 0
+    polynomial = sympy.sympify(0)
     n = len(x_values)
     x = symbols("x")
     for i in range(n):
@@ -55,11 +50,11 @@ def lagrange_polynomial(x_values, y_values, precision=4):
             lagrange *= (x - x_values[j]) / (x_values[i] - x_values[j])
         polynomial += y_values[i] * lagrange
     if contains_float:
-        return str(sympy.sympify(sympy.cancel(polynomial, x), rational=None).evalf(n=precision)).replace("**", "^")
+        return str(sympy.nsimplify(polynomial.evalf().cancel(),  tolerance=10**-precision)).replace("**", "^")
     return str(sympy.cancel(polynomial, x)).replace("**", "^")
 
 
-def berruta_functions(x_values, y_values, precision=4):
+def berruta_functions(x_values, y_values, precision=2):
     x_values, y_values, contains_float = cast_to_number(x_values, y_values)
     numerator = 0
     denominator = 0
@@ -72,8 +67,8 @@ def berruta_functions(x_values, y_values, precision=4):
                 continue
             product *= (x - x_values[i])
         numerator += (-1) ** k * y_values[k] * product
-        denominator += (-1) ** k * product
-    polynomial = numerator / denominator
+        denominator += ((-1) ** k * product)
+    polynomial = sympy.sympify(numerator/denominator)
     if contains_float:
-        return str(sympy.sympify(sympy.cancel(polynomial, x), rational=None).evalf(n=precision)).replace("**", "^")
-    return str(sympy.cancel(polynomial, x)).replace("**", "^")
+        return str(sympy.nsimplify(polynomial.evalf().cancel(), tolerance=10**-precision)).replace("**", "^")
+    return str(polynomial.cancel()).replace("**", "^")
